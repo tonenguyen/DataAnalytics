00ea19f3c1fc69745180ee154a778c9c4a0accad
ToanDNguyen <tonednguyen@gmail.com>
2019-03-11 18:04:23 -0700

    HW 3 - Add computation for great de/increase based on values and indexes.

--- HW3/PyBank/financialAnalysis.py
+++ HW3/PyBank/financialAnalysis.py
@@ -1,6 +1,8 @@
+#Author: Toan D Nguyen
+#HW3 part A
+
 import os
 import csv
-
 
 #csvpath = os.path.join('\Resources', 'budget_data.csv')
 csvpath = r'Resources\budget_data.csv'
@@ -19,17 +21,24 @@ with open(financialCSV, 'r') as csvfile:
         date = row[0]
         amount = int(row[1])
         dataSet[date] = amount
-print(dataSet)
+#print(dataSet)
 #print(len(dataSet.keys()))
-totalAmount = 0
-for eachAmount in dataSet.values():
-    totalAmount = totalAmount + eachAmount
-transacts = [transact for transact in dataSet.values()]
+totalAmount = sum(dataSet.values())
+transacts = list(dataSet.values())
 #print(transacts)
 diffDataSet = [(b - a) for a, b in zip(transacts[:-1], transacts[1:]) ]
 diffAverage = sum(diffDataSet)/len(diffDataSet)
 #print(diffAverage)
 numTransactions = len(dataSet)
 totalAverage = totalAmount/numTransactions
-print(f"{numTransactions} transactions {totalAmount} {diffAverage} ")
+print(" Financial Analysis ")
+print(" ---------------------------- " )
+print(f" Total months {numTransactions} \n Total: ${totalAmount} \n Avg Change: {diffAverage:.2f} ")
+# find the key greatIn/Decrease through determining index of min/max values from dataSet.values()
+greatDecreaseKey = list(dataSet.keys())[transacts.index(min(transacts))]
+greatIncreaseKey = list(dataSet.keys())[transacts.index(max(transacts))]
+
+print(f" Great Decrease in Profit: {greatDecreaseKey} (${dataSet[greatDecreaseKey]})")
+print(f" Great Increase in Profit: {greatIncreaseKey} (${dataSet[greatIncreaseKey]}) ")
+
     
